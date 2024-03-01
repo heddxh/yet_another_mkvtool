586e4a4796c4de85ed58f6d82603727c67d823af
Heddxh <g311571057@gmail.com>
2024-03-01 21:11:48 +0800

    Drop multiple subtitle tracks support

--- sub_helper.py
+++ sub_helper.py
@@ -12,7 +12,7 @@ output_dir = "/home/heddxh/Workspace/Resources/yet_another_mkvtool/example/outpu
 
 # 字幕文件名语言标识
 SC: str = "sc"  # 简体中文
-TC: str = "tc"  # 繁体中文
+TC: str = "tc"  # TODO: 繁体中文，暂时只处理一条轨道
 
 
 def main():
@@ -35,7 +35,7 @@ def main():
                 for entry in ot:
                     if entry.is_dir() and entry.name.endswith("subsetted"):
                         exec += merge_fonts(entry.path)
-            print(f"将执行: {exec}")
+            # print(f"将执行: {exec}")
             subprocess.run(exec)
 
 
@@ -43,7 +43,8 @@ def find_sub(main_name: str) -> List[str]:
     """查找字幕文件并返回绝对路径列表"""
     result: List[str] = []
     for filename in os.listdir(sub_dir):
-        if filename.startswith(main_name) and filename.endswith("ass"):
+        # if filename.startswith(main_name) and filename.endswith("ass"):
+        if filename.startswith(main_name) and filename.endswith("sc.ass"): # TODO: 目前只处理简体中文
             print(f"{main_name} -> {filename}")
             result.append(os.path.join(sub_dir, filename))
     return result
@@ -54,18 +55,16 @@ def merge_sub(sub_path: str, mkv_path: str) -> List[str]:
     filename, _ = os.path.splitext(base_name)
     _, lan = os.path.splitext(filename)
     lan = lan[1:]
-    output_name, _ = os.path.splitext(os.path.basename(mkv_path))
-    output_path = os.path.join(output_dir, output_name) + ".mkv"
     # 判断简繁
     if lan == SC:
         language_code = "zh-Hans"
         track_name = "简体中文"
-    elif lan == TC:
-        language_code = "zh-Hant"
-        track_name = "繁体中文"
+    # elif lan == TC:
+    #    language_code = "zh-Hant"
+    #    track_name = "繁体中文"
     else:
-        print(lan)
-        raise
+        print("fPass: {lan}")
+        return []
     # 嵌入字幕
     return [
         mkv_path,
